e7fa54d0f18d4b3e6dbba3cc364e13bf0b6fc048
Serena Wang <serena.happyhacking@gmail.com>
2020-09-08 15:41:52 +0800

    搭建脚手架

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+
+module.exports = {
+    mode: "development",
+    entry: path.resolve(__dirname, "src/index.jsx"),
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
+        publicPath: "/",
+    },
+    module: {
+        rules: [
+            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+            {
+                test: /\.jsx?$/,
+                use: [
+                    {
+                        loader: "babel-loader",
+                        options: {
+                            presets: ["@babel/preset-env", "@babel/react"],
+                            plugins: [
+                                [
+                                    "@babel/plugin-proposal-decorators",
+                                    { legacy: true },
+                                ],
+                            ],
+                        },
+                    },
+                ],
+                include: path.resolve(__dirname, "src"),
+                exclude: /node_modules/,
+            },
+        ],
+    },
+};
+
+// 装一个css loader, style-component loader
