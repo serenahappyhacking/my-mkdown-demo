7db4b877e0ddf94c42b5b0d882318c53d4d459a9
Serena Wang <serena.happyhacking@gmail.com>
2020-09-10 10:28:31 +0800

    add HtmlWebpackPlugin to generate HTML file to serve bundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,41 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: 'development',
-    entry: path.resolve(__dirname, 'src/index.js'),
+    mode: "development",
+    entry: path.resolve(__dirname, "src/index.js"),
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-        publicPath: '/',
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
+        publicPath: "/",
     },
     module: {
         rules: [
-            { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
             {
                 test: /\.jsx?$/,
                 use: [
                     {
-                        loader: 'babel-loader',
+                        loader: "babel-loader",
                         options: {
-                            presets: ['@babel/preset-env', '@babel/react'],
+                            presets: ["@babel/preset-env", "@babel/react"],
                             plugins: [
                                 [
-                                    '@babel/plugin-proposal-decorators',
+                                    "@babel/plugin-proposal-decorators",
                                     { legacy: true },
                                 ],
                             ],
                         },
                     },
                 ],
-                include: path.resolve(__dirname, 'src'),
+                include: path.resolve(__dirname, "src"),
                 exclude: /node_modules/,
             },
         ],
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "index.html",
+        }),
+    ],
 };
-// 装一个css loader, style-component loader
