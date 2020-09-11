95e292c1985b56ecb65c9b40757992ab0f9f236d
Serena Wang <serena.happyhacking@gmail.com>
2020-09-11 11:08:44 +0800

    add clean-webpack-plugin to clean up the /dist folder beforeeach build

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
     mode: "development",
@@ -38,5 +39,6 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: "index.html",
         }),
+        new CleanWebpackPlugin(),
     ],
 };
